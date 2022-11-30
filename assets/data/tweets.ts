6ac44501cf8166e962ea8744d0c287a339cd01af
Can Kanbur <cankanbur1@gmail.com>
2022-11-30 23:39:08 +0300

    init sample

---
+++ assets/data/tweets.ts
@@ -0,0 +1,69 @@
+const tweets = [
+  {
+    id: 't1',
+    user: {
+      id: 'u1',
+      username: 'irmüş',
+      name: 'İrmik Kedi',
+      image:
+        'https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2F0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
+    },
+    createdAt: '2020-08-27T12:00:00.000Z',
+    content:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    image:
+      'https://images.unsplash.com/photo-1442291928580-fb5d0856a8f1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NjN8fGNhdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60',
+    numberOfComments: 123,
+    numberOfRetweets: 11,
+    numberOfLikes: 10,
+  },
+  {
+    id: 't2',
+    user: {
+      id: 'u1',
+      username: 'irmüş',
+      name: 'İrmik Kedi',
+      image:
+        'https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2F0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
+    },
+    createdAt: '2020-08-27T12:00:00.000Z',
+    content:
+      "Hey Hey Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    numberOfComments: 4,
+    numberOfRetweets: 11,
+    numberOfLikes: 99,
+  },
+  {
+    id: 't3',
+    user: {
+      id: 'u1',
+      username: 'irmüş',
+      name: 'İrmik Kedi',
+      image:
+        'https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2F0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
+    },
+    createdAt: '2020-08-27T12:00:00.000Z',
+    content: 'Hello World',
+    numberOfComments: 4,
+    numberOfRetweets: 11,
+    numberOfLikes: 99,
+  },
+  {
+    id: 't4',
+    user: {
+      id: 'u1',
+      username: 'irmüş',
+      name: 'İrmik Kedi',
+      image:
+        'https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2F0fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
+    },
+    createdAt: '2020-08-27T12:00:00.000Z',
+    content:
+      "Hey Hey Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    numberOfComments: 4,
+    numberOfRetweets: 11,
+    numberOfLikes: 99,
+  },
+];
+
+export default tweets;
